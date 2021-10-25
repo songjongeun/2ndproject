2732ab72c25e79366035b7454007f985e94ed345
ball02meat <89518561+ball02meat@users.noreply.github.com>
2021-10-25 20:18:16 +0900

    Update MockDto.java

--- mock/src/main/java/kr/co/mock/dto/MockDto.java
+++ mock/src/main/java/kr/co/mock/dto/MockDto.java
@@ -3,9 +3,7 @@ package kr.co.mock.dto;
 public class MockDto {
 	private int m_id, mileage;
 	private String userid, start, end;
-	private String userid,pwd,username,email,m_apply;
 
-	// 모의투자
 	public int getM_id() {
 		return m_id;
 	}
@@ -37,46 +35,5 @@ public class MockDto {
 		this.end = end;
 	}
 
-	// 사용자
-	public String getUserid() {
-		return userid;
-	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getM_apply() {
-		return m_apply;
-	}
-
-	public void setM_apply(String m_apply){
-		this.m_apply= m_apply;
-	}
-	
 }
 
