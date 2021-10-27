7b97e85fcb85d4ec204695e1a0ae6a4f23a01507
ball02meat <89518561+ball02meat@users.noreply.github.com>
2021-10-27 18:47:00 +0900

    Update UserDto.java

--- mock/src/main/java/kr/co/mock/dto/UserDto.java
+++ mock/src/main/java/kr/co/mock/dto/UserDto.java
@@ -1,8 +1,17 @@
-package kr.co.user.dto;
+package kr.co.mock.dto;
 
 public class UserDto {
+//  사용자 user
 	private String userid,pwd,username,email,m_apply;
+// 관심종목interests  관심id 종목코드                userid로 찾기
+	private String i_id,code;
+// 종목 stocks       종목명   대비        거래량       code로 찾기
+	private String name,diff,vol;
+// 실시간종목 realtime  시간id 현재가                  code로 찾기
+	private String r_id,cprice;
 
+
+	// 사용자
 	public String getUserid() {
 		return userid;
 	}
@@ -41,6 +50,65 @@ public class UserDto {
 
 	public void setM_apply(String m_apply){
 		this.m_apply= m_apply;
+	}
+
+	
+	// 관심종목
+
+	public String getI_id() {
+		return i_id;
+	}
+
+	public void setI_id(String i_id) {
+		this.i_id = i_id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDiff() {
+		return diff;
+	}
+
+	public void setDiff(String diff) {
+		this.diff = diff;
+	}
+
+	public String getVol() {
+		return vol;
+	}
+
+	public void setVol(String vol) {
+		this.vol = vol;
+	}
+
+	public String getR_id() {
+		return r_id;
+	}
+
+	public void setR_id(String r_id) {
+		this.r_id = r_id;
+	}
+
+	public String getCprice() {
+		return cprice;
+	}
+
+	public void setCprice(String cprice) {
+		this.cprice = cprice;
 	}
 	
 }
