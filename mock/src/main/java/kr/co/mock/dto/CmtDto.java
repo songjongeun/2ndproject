98be3d5a50d92d9cd7020dbd7d19887fecf6d43a
plzmefree <91742483+plzmefree@users.noreply.github.com>
2021-11-04 21:00:34 +0900

    Create CmtDto.java

---
+++ mock/src/main/java/kr/co/mock/dto/CmtDto.java
@@ -0,0 +1,38 @@
+package kr.co.mock.dto;
+
+public class CmtDto {
+	
+		private int c_id,f_id;
+		private String userid,content,writeday;
+		
+		public int getC_id() {
+			return c_id;
+		}
+		public void setC_id(int c_id) {
+			this.c_id = c_id;
+		}
+		public int getF_id() {
+			return f_id;
+		}
+		public void setF_id(int f_id) {
+			this.f_id = f_id;
+		}
+		public String getUserid() {
+			return userid;
+		}
+		public void setUserid(String userid) {
+			this.userid = userid;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+		public String getWriteday() {
+			return writeday;
+		}
+		public void setWriteday(String writeday) {
+			this.writeday = writeday;
+		}
+}
